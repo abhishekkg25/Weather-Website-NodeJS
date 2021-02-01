effcaf4caf28797600ad9a1dc1a6ac07bde3a59c
Abhishek Gupta <abhigupta.master@gmail.com>
2021-02-01 22:31:37 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const app = express()
+
+const directoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(directoryPath))
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title: 'hey siri',
+        name: 'Abhishek'
+    })
+} )
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Kanpur',
+        address: req.query.address
+    })
+})
+
+app.get('/get', (req, res) => {
+    res.send('hey there!')
+ })
+
+app.listen(3000, () => {
+    console.log("Server starts..")
+})
+
